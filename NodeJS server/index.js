1e98785f905160d6d7c5911eab297db2314428ab
Yentl Derumeaux <26748711+YentlD@users.noreply.github.com>
2020-02-15 15:19:13 +0100

    added greenhouses to array

--- NodeJS server/index.js
+++ NodeJS server/index.js
@@ -4,11 +4,11 @@ const mqtt = require(`mqtt`);
   //////////////
  //// VARS ////
 //////////////
+const greenHouses = [];
 const webServerPort = 80;
 const mqttUrl = `mqtt://broker.hivemq.com`
 const mqttPort = `1883`
 const mqttTopic = `/DeCodeBadgers`
-
 
 const mqttClient = mqtt.connect(`${mqttUrl}:${mqttPort}`)
 const server = express();
@@ -18,20 +18,45 @@ const router = express.Router();
  //// MQTT CLIENT ////
 /////////////////////
 const mqttClientConnect = async function(){
-    return new Promise(function(resolve, reject){
-        mqttClient.on('connect', function(){
-            mqttClient.subscribe(mqttTopic, function(e){
-                if(e){
-                    console.log(e.message);
-                    reject();
-                } else {
-                    mqttClient.publish(mqttTopic, `API has made connection`);
-                    console.log(`MQTT connection made to ${mqttUrl} on Topic: ${mqttTopic}`)
-                    resolve();
-                }
-            })
+    mqttClient.on('connect', function(){
+        mqttClient.subscribe(`${mqttTopic}/#`, function(e){
+            if(e){
+                console.log(e.message);
+                reject();
+            } else {
+                mqttClient.publish(mqttTopic, `API has made connection`);
+                console.log(`MQTT connection made to ${mqttUrl} on Topic: ${mqttTopic}`)
+                mqttClient.on('message', function(topic, message){
+                    if(topic == `/DeCodeBadgers/API`){
+                        let data = JSON.parse(message.toString());
+                        addDataToArray(data)
+
+                    } else {
+                        console.log(topic, message.toString());
+                    }
+                })
+            }
         })
     })
+}
+
+const addDataToArray = function(data){
+    let hasFound = false;
+    greenHouses.forEach(greenHouse => {
+        if(greenHouse.uniqueId === data.uniqueId){
+            greenHouse.light = data.light;
+            greenHouse.humidity = data.humidity;
+            greenHouse.temperature = data.temperature;
+            greenHouse.doorOpen = data.doorOpen;
+
+            hasFound = true;
+        }
+    });
+    if(!hasFound){
+        greenHouses.push(data);
+    }
+    console.log(greenHouses);
+
 }
 
 
